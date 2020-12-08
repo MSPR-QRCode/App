41b86d54e36ba3c8b33deac9bd2cdd9a82665b57
Corentin Lissillour <corentin@moonshuttle.com>
2020-12-08 14:37:11 +0100

    DELETE CONTENU APP

--- src/styles/color.js
+++
@@ -1,6 +0,0 @@
-export const blue = "#003049";
-export const red = "#D62828"; 
-export const grey = "#CFD2D7"; 
-export const orange = "#F77F00"; 
-export const yellow = "#FCBF49";
-export const meringue = "#EAE2B7"; 
