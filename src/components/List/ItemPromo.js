1fab18f2e7f999a06d1b18d8f82e9a1f615c1438
MorganMBA <mba@intechinfo.fr>
2020-12-19 13:37:02 +0100

    vue list promo

--- src/components/List/ItemPromo.js
+++ src/components/List/ItemPromo.js
@@ -27,6 +27,7 @@ const styles = StyleSheet.create({
       },
       fontFamily:  "Montserrat-Medium"
     },  
+    //
     title: {
         fontSize: 24,
         fontFamily:  "Montserrat-ExtraBold"  
