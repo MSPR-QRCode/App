66040362a460b81d396429f7079d8e4ab71b2237
MorganMBA <mba@intechinfo.fr>
2020-12-19 13:30:43 +0100

    vues des lists promo

---
+++ src/components/List/ItemPromo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const ItemPromo = ({item}) => {
+   return (
+    <TouchableOpacity style={styles.item}>
+    <Text style={styles.title}>{item.name}</Text>
+    <Text >Date limite : {item.date}</Text>
+    <Text style={styles.message}>{item.message}</Text>
+  </TouchableOpacity>
+   ); 
+}
+
+const styles = StyleSheet.create({
+    item: {
+    //   backgroundColor: "red",
+      margin: 10,
+      borderRadius: 5,
+      padding: 5,
+      shadowOpacity: 0.23,
+      shadowRadius: 2.62,
+      elevation: 4,
+      shadowColor: "#000",
+      shadowOffset: {
+      width: 0,
+        height: 3,
+      },
+      fontFamily:  "Montserrat-Medium"
+    },  
+    title: {
+        fontSize: 24,
+        fontFamily:  "Montserrat-ExtraBold"  
+    },
+    message: {
+      fontSize: 20,
+      fontFamily:  "Montserrat-Regular"  
+  }
+  })
+
+export default ItemPromo;
---
+++ src/components/List/List.js
@@ -0,0 +1,23 @@
+//Import React 
+import React from 'react';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
+import { fakePromo } from '../../services/FakePromo';
+import ItemPromo from './ItemPromo';
+  
+const List = () => {
+    return(
+        <FlatList  style={styles.list}
+        data={fakePromo}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (  <ItemPromo item={item} />)}
+      />
+    );
+}
+
+const styles = StyleSheet.create({
+    list: {
+      flex: 1
+    }
+  })
+
+export default List; 
