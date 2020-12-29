c9e2ff4ac310e71efc7fe41acc717ab22e948078
MorganMBA <mba@intechinfo.fr>
2020-12-29 11:23:16 +0100

    list promo

--- src/components/List/ItemPromo.js
+++ src/components/List/ItemPromo.js
@@ -28,6 +28,7 @@ const styles = StyleSheet.create({
       fontFamily:  "Montserrat-Medium"
     },  
     //
+    
     title: {
         fontSize: 24,
         fontFamily:  "Montserrat-ExtraBold"  
