db18ea344ca456b43ce1f0f964ce48c4d79e5398
Aymerick Michelet <aymerick.michelet@epsi.fr>
2021-03-28 16:38:25 +0200

    wip ca marche pas

---
+++ e2e/Navigation.e2e.js
@@ -0,0 +1,12 @@
+// import Navigation from '../src/navigations';
+
+describe('Navigation', () => {
+    beforeEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    // it('should go to Promotion', async () => {
+    //     await expect(element(by.text('Promotions'))).toBeVisible();
+    // });
+    it('should go to Promotion', async () => {});
+});
